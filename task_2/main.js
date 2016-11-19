11cc3bc33400d756d7810ebb6eff00e882a0b359
YuliiaKravchenko <ylechk@inbox.ru>
2016-11-20 01:06:38 +0200

    change array in to string in second option task 2

--- task_2/main.js
+++ task_2/main.js
@@ -13,11 +13,14 @@ console.log(checkArgType1(1,"hi",a=[1,2,3],true));
 //2 variant
 function checkAgrType2(...theArgs){
    let result = [];
+    let str;
     for (var i=0; i<theArgs.length; i++){
         result[i]=typeof(theArgs[i]);
+        str=result.join(',');
     }
-    return result;
+    return str;
 }
 
 console.log(checkAgrType2(5, true, {a:"letter"}));
+
 
